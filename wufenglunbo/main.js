8cdbab19b38014459c2210b52e1f74577f5d15a4
r0909 <43103674+r0909@users.noreply.github.com>
2018-09-26 13:48:56 +0800

    Update main.js

--- wufenglunbo/main.js
+++ wufenglunbo/main.js
@@ -1,17 +1,16 @@
-let n
-初始化()
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+let n = 1
 setInterval(()=>{
-  makeLeave(getImage(n))
+  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
     .one('transitionend', (e)=>{
-      makeEnter($(e.currentTarget))
+      $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-  makeCurrent(getImage(n+1))
+  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
   n += 1
 },3000)
 
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
-}
 
 function x(n){
   if(n>3){
@@ -21,20 +20,4 @@ function x(n){
     }
   } // n = 1 2 3
   return n
-}
-
-function 初始化(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
 }
